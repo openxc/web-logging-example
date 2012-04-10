23b437d2bb1a62cbaa3a5c2856551f9565fe501a
Christopher Peplin <cpeplin@ford.com>
2012-04-10 11:37:10 -0400

    Add code to draw the graph.

--- static/js/app.js
+++ static/js/app.js
@@ -1,2 +1,83 @@
+var options = {
+    chart: {
+       renderTo: 'chart'
+    },
+    legend: {
+        enabled: false
+    },
+    title: {
+       text: 'Average Fuel Economy'
+    },
+    xAxis: {
+       type: 'datetime',
+       tickInterval: 24 * 3600 * 1000, // one day
+       gridLineWidth: 1,
+       labels: {
+          align: 'left',
+          x: 3,
+          y: -3
+       }
+    },
+    yAxis: {
+       title: {
+          text: null
+       },
+       labels: {
+          align: 'left',
+          x: 3,
+          y: 16,
+          formatter: function() {
+             return Highcharts.numberFormat(this.value, 0);
+          }
+       },
+       showFirstLabel: false
+    },
+    tooltip: {
+       shared: true,
+       crosshairs: true
+    },
+    plotOptions: {
+       series: {
+          cursor: 'pointer',
+          point: {
+             events: {
+                click: function() {
+                   hs.htmlExpand(null, {
+                      pageOrigin: {
+                         x: this.pageX,
+                         y: this.pageY
+                      },
+                      headingText: this.series.name,
+                      maincontentText: Highcharts.dateFormat(
+                          '%A, %b %e, %Y', this.x) + ':<br/> ' + this.y +
+                          ' visits',
+                      width: 200
+                   });
+                }
+             }
+          },
+          marker: {
+             lineWidth: 1
+          }
+       }
+    }
+}
+
 $(document).ready(function() {
+    $.get($("#chart").data('url'), function(data) {
+        var series = [];
+        $.each(data.records, function(i, record) {
+            series.push([record.timestamp, record.value]);
+        });
+
+        options.series = [];
+        options.series.push({name: "Ford Focus",
+            lineWidth: 4,
+            market: {
+                radius: 4
+            },
+            data: series});
+
+        chart = new Highcharts.Chart(options);
+    }, 'json');
 });
