bd50509999d1866f8a53861b1db37cdda79795d3
Christopher Peplin <cpeplin@ford.com>
2012-04-12 11:12:19 -0400

    Add a workaround to print import errors when running tests.
    This is due to a bug in unittest.

--- tests/run_tests.py
+++ tests/run_tests.py
@@ -6,8 +6,17 @@ TEST_MODULES = [
     'tests.test_visualization',
 ]
 
+
 def all():
-    return unittest.defaultTestLoader.loadTestsFromNames(TEST_MODULES)
+    try:
+        return unittest.defaultTestLoader.loadTestsFromNames(TEST_MODULES)
+    except AttributeError, e:
+        if "'module' object has no attribute 'test_" in str(e):
+            # most likely because of an import error
+            for m in TEST_MODULES:
+                __import__(m, globals(), locals())
+        raise
+
 
 if __name__ == '__main__':
     import tornado.testing
