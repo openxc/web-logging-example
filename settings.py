279e75a18faf06e73634da37237e23274e13ae14
Christopher Peplin <cpeplin@ford.com>
2012-04-12 11:22:00 -0400

    Change default port to 5000 to match existing Java client.

--- settings.py
+++ settings.py
@@ -12,7 +12,7 @@ import logconfig
 path = lambda root,*a: os.path.join(root, *a)
 ROOT = os.path.dirname(os.path.abspath(__file__))
 
-define("port", default=8888, help="run on the given port", type=int)
+define("port", default=5000, help="run on the given port", type=int)
 define("config", default=None, help="tornado config file")
 define("debug", default=False, help="debug mode")
 tornado.options.parse_command_line()
