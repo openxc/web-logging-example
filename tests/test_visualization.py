cadab3fef1e95c0b186dc28860ac4797243fdf22
Christopher Peplin <cpeplin@ford.com>
2012-04-12 15:11:41 -0400

    Update test case for visualization page.

--- tests/test_visualization.py
+++ tests/test_visualization.py
@@ -15,4 +15,4 @@ class VisualizationTestCase(BaseRecorderTestCase):
     def test_no_data(self):
         self.http_client.fetch(self.get_url('/visualization'), self.stop)
         response = self.wait()
-        assert 'class="vehicle"' in response.body
+        assert "measurement" in response.body
