0bfa2e3696194949fa177b51073e3fd19bc94a32
Christopher Peplin <chris.peplin@rhubarbtech.com>
2013-09-13 08:33:35 -0400

    Sort filenames by modification time in the list.

--- handlers/visualization.py
+++ handlers/visualization.py
@@ -8,6 +8,9 @@ logger = logging.getLogger('recorder.' + __name__)
 
 class VisualizationHandler(BaseHandler):
     def get(self):
-        trace_filenames = [filename for filename in
-                os.listdir(settings['trace_folder'])]
+        trace_filenames = [os.path.join(settings['trace_folder'], filename)
+                for filename in os.listdir(settings['trace_folder'])]
+        trace_filenames.sort(key=lambda x: os.path.getmtime(x))
+        trace_filenames = [trace_filename.split('/')[1] for trace_filename in
+                trace_filenames]
         self.render("visualization.html", trace_filenames=trace_filenames)
